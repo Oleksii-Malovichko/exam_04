ccf784d15ab3af0b9bc1a3f16b26328538f4d1dc
alex <alex@dev-kate-log>
2025-09-04 14:57:35 +0200

    learn picoshell

--- exam_changed/ex00/picoshell/picoshell2.c
+++ exam_changed/ex00/picoshell/picoshell2.c
@@ -39,7 +39,7 @@ int picoshell(char **cmds[])
 			if (cmds[i + 1])
 			{
 				close(fds[0]);
-				dup2(fds[2], STDOUT_FILENO);
+				dup2(fds[1], STDOUT_FILENO);
 				close(fds[1]);
 			}
 			execvp(cmds[i][0], cmds[i]);
@@ -62,14 +62,55 @@ int picoshell(char **cmds[])
 	return exit_code;
 }
 
-int main(void)
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+static int	count_cmds(int argc, char **argv)
 {
-    char *echo[] = {"/bin/echo", "hello world", NULL};
-    char *grep[] = {"/bin/grep", "world", NULL};
-    char **cmds[] = {echo, grep, NULL};
-
-    printf("=== TEST 2: echo | grep ===\n");
-    int ret = picoshell(cmds);
-    printf("Return code: %d\n\n", ret);
-    return 0;
+	int	count = 1;
+	for (int i = 1; i < argc; i++)
+	{
+		if (strcmp(argv[i], "|") == 0)
+			count++;
+	}
+	return (count);
 }
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (fprintf(stderr, "Usage: %s cmd1 [args] | cmd2 [args] ...\n", argv[0]), 1);
+
+	int	cmd_count = count_cmds(argc, argv);
+	char	***cmds = calloc(cmd_count + 1, sizeof(char **));
+	if (!cmds)
+		return (perror("calloc"), 1);
+
+	int	i = 1, j = 0;
+	while (i < argc)
+	{
+		int	len = 0;
+		while (i + len < argc && strcmp(argv[i + len], "|") != 0)
+			len++;
+		cmds[j] = calloc(len + 1, sizeof(char *));
+		if (!cmds[j])
+			return (perror("calloc"), 1);
+		for (int k = 0; k < len; k++)
+			cmds[j][k] = argv[i + k];
+		cmds[j][len] = NULL;
+		i += len + 1;
+		j++;
+	}
+	cmds[cmd_count] = NULL;
+
+	int	ret = picoshell(cmds);
+
+	// Clean up
+	for (int i = 0; cmds[i]; i++)
+		free(cmds[i]);
+	free(cmds);
+
+	return (ret);
+}
+
+// ./picoshell2.o echo 'squalala' "|" cat "|" sed 's/a/b/g'
